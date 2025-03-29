8e1757c239d8599bc8ad8cd0eb144e0fcc444c41
antoniokho13 <khoantonio03@gmail.com>
2025-03-30 00:55:12 +0800

    Added Product and Category Entities
    Added Product and Category Entities but not yet tested in Postman

---
+++ backend/src/main/java/edu/cit/sapatosan/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package edu.cit.sapatosan.repository;
+
+import edu.cit.sapatosan.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+
+}
---
+++ backend/src/main/java/edu/cit/sapatosan/service/CategoryService.java
@@ -0,0 +1,41 @@
+package edu.cit.sapatosan.service;
+
+import edu.cit.sapatosan.entity.CategoryEntity;
+import edu.cit.sapatosan.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryEntity> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<CategoryEntity> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public CategoryEntity createCategory(CategoryEntity category) {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<CategoryEntity> updateCategory(Long id, CategoryEntity updatedCategory) {
+        return categoryRepository.findById(id).map(category -> {
+            category.setName(updatedCategory.getName());
+            category.setProducts(updatedCategory.getProducts());
+            return categoryRepository.save(category);
+        });
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
