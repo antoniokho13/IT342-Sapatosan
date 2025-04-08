16c3bfba49a70d6e260c9ae8d84cd4c76ba7cebc
antoniokho13 <khoantonio03@gmail.com>
2025-04-08 16:56:06 +0800

    Added Firebase database to support cloud.

---
+++ backend/src/main/java/edu/cit/sapatosan/service/FirebaseInitializer.java
@@ -0,0 +1,29 @@
+package edu.cit.sapatosan.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+public class FirebaseInitializer {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-config.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
